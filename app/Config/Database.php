07e843a1347a1d473d45a419278f0ee59468fa9a
Capio21 <130652957+Capio21@users.noreply.github.com>
2023-10-04 08:29:05 +0800

    1

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'product1',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
